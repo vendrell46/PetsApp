140ce0b4c4ce12b466204ed15ed2defafb3d4527
Carlos Vendrell <carlos.vendrell@tigerspike.com>
2021-01-26 17:09:12 +0100

    Creating Add Activity

--- app/src/main/java/com/carlos/myapps/petsapp/AddActivity.kt
+++ app/src/main/java/com/carlos/myapps/petsapp/AddActivity.kt
@@ -1,6 +1,9 @@
 package com.carlos.myapps.petsapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class AddActivity : AppCompatActivity() {
@@ -8,5 +11,15 @@ class AddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
+
+        val addTaskBtn = findViewById<Button>(R.id.add_btn)
+        addTaskBtn.setOnClickListener {
+            taskAddedMessage()
+        }
+    }
+
+    private fun taskAddedMessage() {
+        val taskNameTitle = findViewById<EditText>(R.id.task_title)
+        Toast.makeText(this, taskNameTitle.text, Toast.LENGTH_SHORT).show()
     }
 }
